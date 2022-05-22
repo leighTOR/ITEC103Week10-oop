4d910c0d703cfe93101fa2678280e89b9145b2ba
leighTOR <leighinna.jamolin@lspu.edu.ph>
2022-05-22 14:54:32 +0800

    fix: Implement try catch block

--- boardmarker/Program.cs
+++ boardmarker/Program.cs
@@ -11,72 +11,79 @@ namespace intermediate
             string msg = string.Empty;
             string userColor = string.Empty;
 
-            while (true)
+            try
             {
-                Console.Clear();
-                Console.WriteLine("[1] Write");
-                Console.WriteLine("[2] Refill");
-                Console.WriteLine("[3] Change Color");
-                Console.WriteLine("[4] Check Status");
-                Console.WriteLine("[5] Dispose");
-                Console.WriteLine("--------------");
-                Console.Write("Command: ");
-                choice = Convert.ToInt32(Console.ReadLine());
-
-                switch (choice)
+                while (true)
                 {
-                    case 1:
-                        Console.Write("\n");
-                        Console.Write("Message: ");
-                        Console.Write("\n");
-                        msg = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("[1] Write");
+                    Console.WriteLine("[2] Refill");
+                    Console.WriteLine("[3] Change Color");
+                    Console.WriteLine("[4] Check Status");
+                    Console.WriteLine("[5] Dispose");
+                    Console.WriteLine("--------------");
+                    Console.Write("Command: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
 
-                        switch (myMarker.color)
-                        {
-                            case "blue":
-                                Console.ForegroundColor =
-                                ConsoleColor.Blue;
-                                break;
-                            case "red":
-                                Console.ForegroundColor =
-                                ConsoleColor.Red;
-                                break;
-                        }
-                        Console.Write("\n");
-                        myMarker.Write(msg);
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.ReadKey(true);
-                        break;
-                    case 2:
-                        Console.WriteLine();
-                        myMarker.Refill();
-                        Console.ReadKey(true);
-                        break;
-                    case 3:
-                        Console.WriteLine();
-                        Console.WriteLine("Choose from: White, Blue and Red");
-                        Console.Write("Color: ");
-                        userColor = Console.ReadLine();
-                        Console.Write("\n");
-                        myMarker.ChangeColor(userColor);
-                        Console.ReadKey(true);
-                        break;
-                    case 4:
-                        Console.WriteLine();
-                        myMarker.CheckStatus();
-                        Console.ReadKey(true);
-                        break;
-                    case 5:
-                        Console.WriteLine();
-                        myMarker.Dispose();
-                        Console.ReadKey(true);
-                        break;
-                    default:
-                        Console.WriteLine();
-                        Console.WriteLine("Invalid command...");
-                        Console.ReadKey(true);
-                        break;
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("\n");
+                            Console.Write("Message: ");
+                            Console.Write("\n");
+                            msg = Console.ReadLine();
+
+                            switch (myMarker.color)
+                            {
+                                case "blue":
+                                    Console.ForegroundColor =
+                                    ConsoleColor.Blue;
+                                    break;
+                                case "red":
+                                    Console.ForegroundColor =
+                                    ConsoleColor.Red;
+                                    break;
+                            }
+                            Console.Write("\n");
+                            myMarker.Write(msg);
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.ReadKey(true);
+                            break;
+                        case 2:
+                            Console.WriteLine();
+                            myMarker.Refill();
+                            Console.ReadKey(true);
+                            break;
+                        case 3:
+                            Console.WriteLine();
+                            Console.WriteLine("Choose from: White, Blue and Red");
+                            Console.Write("Color: ");
+                            userColor = Console.ReadLine();
+                            Console.Write("\n");
+                            myMarker.ChangeColor(userColor);
+                            Console.ReadKey(true);
+                            break;
+                        case 4:
+                            Console.WriteLine();
+                            myMarker.CheckStatus();
+                            Console.ReadKey(true);
+                            break;
+                        case 5:
+                            Console.WriteLine();
+                            myMarker.Dispose();
+                            Console.ReadKey(true);
+                            break;
+                        default:
+                            Console.WriteLine();
+                            Console.WriteLine("Invalid command...");
+                            Console.ReadKey(true);
+                            break;
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
         }
     }
